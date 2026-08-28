0b7240a0660ff02752e4a00562b544408d949f74
meirelesnew <muty.bjj@gmail.com>
2026-08-28 10:35:22 -0300

    Fix syntax error in service worker event listeners

--- sw.js
+++ sw.js
@@ -6,14 +6,12 @@ const ASSETS = [
   './manifest.json'
 ];
 
-// Instala e armazena os arquivos base em cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Intercepta requisições para entregar do cache quando offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
